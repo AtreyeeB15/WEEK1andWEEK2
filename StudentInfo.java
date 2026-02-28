490b15db20a15befa8e34a31c1eaafdf33b6a066
AtreyeeB15 <atreyeebarick@gmail.com>
2026-02-28 16:43:38 +0530

    Practice Problems

---
+++ StudentInfo.java
@@ -0,0 +1,22 @@
+/**
+ * Program Name: StudentInfo
+ * Description: Displays student details using proper comments.
+ */
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+
+        // Declaring variables
+        String studentName = "Atreyee";
+        int studentAge = 20;
+        char studentGrade = 'A';
+
+        /*
+         Printing student details
+         */
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Grade: " + studentGrade);
+    }
+}
