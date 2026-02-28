e428651617adbfc81a65d6d9ab9fa63835e865fa
AtreyeeB15 <atreyeebarick@gmail.com>
2026-02-28 17:35:59 +0530

    Added Datatypes program

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int rollNumber = 101;
+        double height = 5.9;
+        char section = 'B';
+        boolean isPassed = true;
+
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Height: " + height);
+        System.out.println("Section: " + section);
+        System.out.println("Passed: " + isPassed);
+    }
+}
